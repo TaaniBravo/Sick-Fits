ed2e7f65b1f33906d60ee32beca38a81c04fc106
Taani Maama <taani1maama@live.com>
2021-04-01 13:10:04 -0700

    Created our keystone server.

---
+++ backend/keystone.ts
@@ -0,0 +1,32 @@
+// Import files needed.
+import "dotenv/config";
+import { config, createSchema } from "@keystone-next/keystone/schema"
+
+const MONGODB = process.env.MONGODB_URI || "mongodb://localhost/keystone-sick-fits";
+
+const sessionConfig = {
+    maxAge: 60*60*24*360, // how long should they say signed in?
+    secret: process.env.COOKIE_SECRET
+};
+
+export default config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true
+        }
+    },
+    db: {
+        adapter: 'mongoose',
+        url: MONGODB,
+        // TODO add data seeding.
+    },
+    lists: createSchema({
+        // schema items go in here.
+    }),
+    ui: {
+        // todo change this for roles
+        isAccessAllowed: () => true
+    },
+    // add session values here.
+})
