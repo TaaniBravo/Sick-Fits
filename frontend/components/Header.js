cde658638a65ee448c5c851ec8e7fbec5fdc9118
Taani Maama <taani1maama@live.com>
2021-03-31 20:14:30 -0700

    Created the Logo Component.

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,14 +1,32 @@
 import React from "react";
 import Link from "next/link";
 import Nav from "./Nav";
+import styled from "styled-components";
+
+const Logo = styled.h1`
+  background: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 const Header = () => {
   return (
     <header>
-      <div className='bar'>
-        <Link href='/'>Sick Fits</Link>
+      <div className="bar">
+        <Logo>
+          <Link href="/">Sick Fits</Link>
+        </Logo>
       </div>
-      <div className='sub-bar'>
+      <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
