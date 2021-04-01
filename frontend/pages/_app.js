85eef71d957267323c1b838f8f2c2313714c6aff
Taani Maama <taani1maama@live.com>
2021-03-31 21:31:10 -0700

    Updated the rendering bar so as pages load there is a tell.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,12 @@
 import React from "react";
 import Page from "../components/Page";
+import Nprogress from "nprogress";
+import Router from "next/router";
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => Nprogress.start());
+Router.events.on("routeChangeComplete", () => Nprogress.done());
+Router.events.on("routeChangeError", () => Nprogress.done());
 
 const App = ({ Component, pageProps }) => {
   return (
