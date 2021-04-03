4df7a7c2bda59165067e335b78b91a95dd9c468e
Taani Maama <taani1maama@live.com>
2021-04-02 20:40:56 -0700

    Fogot to save.

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,30 +1,7 @@
-import { useQuery } from "@apollo/client";
-import gql from "graphql-tag";
 import React from "react";
 import Products from "../components/Products";
 
-const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
-      id
-      name
-      price
-      description
-      photo {
-        id
-        image {
-          publicUrlTransformed
-        }
-      }
-    }
-  }
-`;
-
 const products = () => {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
-  console.log(data, error, loading);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
   return (
     <div>
       <Products />
