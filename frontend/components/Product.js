633631f83b861f806868e815b4a7d62b6ba9e2e0
Taani Maama <taani1maama@live.com>
2021-04-05 11:29:27 -0700

    Functional deleteProduct

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -3,6 +3,7 @@ import Item from "./styles/ItemStyles";
 import Title from "./styles/Title";
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
+import DeleteProduct from "./DeleteProduct";
 
 const Product = ({ product }) => {
   return (
@@ -27,6 +28,7 @@ const Product = ({ product }) => {
         >
           Edit ✏️
         </Link>
+        <DeleteProduct id={product.id}>Delete</DeleteProduct>
       </div>
     </Item>
   );
