f185daf3ebc7b8510e3c7b162844c72fd3ced6c0
Taani Maama <taani1maama@live.com>
2021-04-04 22:44:59 -0700

    Update Product page is working.

---
+++ frontend/components/UpdateProduct.js
@@ -0,0 +1,106 @@
+import { useMutation, useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import React from "react";
+import useForm from "../lib/useForm";
+import DisplayError from "./ErrorMessage";
+import { SINGLE_ITEM_QUERY } from "./SingleProduct";
+import Form from "./styles/Form";
+
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { id: $id, name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+const UpdateProduct = ({ id }) => {
+  // use our queries and mutations
+  const { data, error, loading } = useQuery(SINGLE_ITEM_QUERY, {
+    variables: {
+      id
+    }
+  });
+
+  if (loading) return <p>Loading</p>;
+  if (error) return <DisplayError error={error} />;
+
+  const [
+    updateProduct,
+    { data: updateData, error: updateError, loading: updateLoading }
+  ] = useMutation(UPDATE_PRODUCT_MUTATION, {
+    variables: {
+      id
+    }
+  });
+
+  //   Call on our useForm states.
+  const { inputs, handleChange, resetForm, clearForm } = useForm(data.Product);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    // const res = await createProduct();
+    // clearForm();
+    // // Go to that product's page!
+    // console.log(res);
+    // Router.push({
+    //   pathname: `/product/${res.data.createProduct.id}`
+    // });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <DisplayError error={error || updateError} />
+      <fieldset disabled={updateLoading} aria-busy={updateLoading}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            type="text"
+            id="description"
+            name="description"
+            placeholder="..."
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
+  );
+};
+
+export default UpdateProduct;
