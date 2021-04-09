5b9d7325e57fa780b297cc3f06b44c659f61c969
Taani Maama <taani1maama@live.com>
2021-04-08 21:36:55 -0700

    Cart Count created.

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,6 +4,7 @@ import NavStyles from "./styles/NavStyles";
 import { useUser } from "./User";
 import SignOut from "./SignOut";
 import { useCart } from "../lib/cartState";
+import CartCount from "./CartCount";
 
 const Nav = () => {
   const user = useUser();
@@ -20,6 +21,9 @@ const Nav = () => {
           <SignOut />
           <button type="button" onClick={openCart}>
             My Cart
+            <CartCount
+              count={user.cart.reduce((acc, item) => acc + item.quantity, 0)}
+            />
           </button>
         </>
       ) : (
